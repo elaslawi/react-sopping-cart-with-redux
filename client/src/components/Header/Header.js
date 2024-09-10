4e153f323b7a30153c3f6655086e7ecb4af9304a
Aslawi-dev <82208438+elaslawi@users.noreply.github.com>
2024-09-10 23:28:05 +0200

    header-comp

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "../../css/Header/Header.css";
+
+
+function Header() {
+  return (
+    
+      <header>
+        <h1>S H O P P I N G    - C A R T  </h1>
+      </header>
+    
+  )
+}
+
+export default Header;
